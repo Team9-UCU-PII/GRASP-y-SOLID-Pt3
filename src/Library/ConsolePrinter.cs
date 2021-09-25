848626341fea4906066b027c050ca704fc7718b9
Marcelo Arrarte <32116762+MarceloArrarte@users.noreply.github.com>
2021-09-25 12:46:00 -0300

    Clase ConsolePrinter creada y Program modificado para usar los nuevos tipos

---
+++ src/Library/ConsolePrinter.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Full_GRASP_And_SOLID.Library {
+    public class ConsolePrinter : IPrinter {
+        public void PrintRecipe(Recipe recipe) {
+            Console.WriteLine(recipe.GetTextToPrint());
+        }
+    }
+}
